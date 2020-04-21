4efb2dc756a6e078cee59eb511ec6f982a693a65
Pooja Singh <pooja.singh@litmusworld.com>
2020-04-21 21:56:20 +0530

    Integrated webservice to implement pgp api call

--- flutter_to_native_communication/ios/Runner/AppDelegate.m
+++ flutter_to_native_communication/ios/Runner/AppDelegate.m
@@ -1,21 +1,30 @@
 #import "AppDelegate.h"
 #import "GeneratedPluginRegistrant.h"
+#import "WebViewJavascriptBridge.h"
 
 #import <Flutter/Flutter.h>
 
+@interface AppDelegate()
+@property WebViewJavascriptBridge* bridge;
+@end
+
 @implementation AppDelegate
+
+NSString *const channelName = @"com.LW2/app";
+
+
 - (BOOL)application:(UIApplication*)application didFinishLaunchingWithOptions:(NSDictionary*)launchOptions {
+
     //1. Controller
     FlutterViewController* controller = (FlutterViewController*)self.window.rootViewController;
     //2. Channel
     FlutterMethodChannel* flutterChannel = [FlutterMethodChannel
-                                            methodChannelWithName:@"com.LW2/app"
+                                            methodChannelWithName: channelName
                                             binaryMessenger:controller.binaryMessenger];
     //3. Method
     [flutterChannel setMethodCallHandler:^(FlutterMethodCall* call, FlutterResult result) {
         // Note: this method is invoked on the UI thread.
         if ([@"encryptData" isEqualToString:call.method]) {
-            
             [self encrpytData:call.arguments completion:^(NSString *data) {
                 result(data);
             }];
@@ -31,12 +40,66 @@
 
 // Method to encrypt data passed from Flutter
 -(void)encrpytData:(NSArray*)dataArray completion:(void (^)(NSString* data))completionBlock {
-    // Input coming from flutter
-    NSLog(@"%@", dataArray);
+    // PGP public key passed from flutter code
+    // Public key is always send at 0th index
+    NSString *pgpPublicKey = dataArray[0];
+    // Input data to be encrypted - passed from flutter code
+    NSDictionary *dataObjectDictionary = @{@"user_name" : @"pooja", @"password" : @"dasa", @"v": @"remember_me",@"p": @"rememberMe"};
     
-    //Output
-    NSString* output = [NSString stringWithFormat:@"%@: %s", dataArray[0], "Data Encrypted"];
-    completionBlock(output);
+    [self fnSetupWebViewJavascriptBridge:dataObjectDictionary pgpPublicKey:pgpPublicKey completion:^(NSString *data) {
+        //Output: - Encrypted data
+        completionBlock(data);
+    }];
+}
+
+-(void)fnSetupWebViewJavascriptBridge:(NSDictionary *)parameters pgpPublicKey:(NSString *)pgpPublicKey completion:(void (^)(NSString* data))completionBlock{
+    
+    UIWindow *window = [[UIApplication sharedApplication] keyWindow];
+    UIWebView *webView;
+    
+    BOOL isWebViewPresentOnWindow = false;
+    for (UIView *subView in [window subviews]) {
+        if ([subView isKindOfClass:[UIWebView class]]) {
+            isWebViewPresentOnWindow = true;
+            webView = (UIWebView *)subView;
+        }
+    }
+    
+    if (!isWebViewPresentOnWindow) {
+        webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+        [window addSubview:webView];
+    }
+    
+   // [WebViewJavascriptBridge enableLogging];
+    WebViewJavascriptBridge* bridge = [WebViewJavascriptBridge bridgeForWebView:webView];
+    
+    [bridge registerHandler:@"pgpEncryptionCallback" handler:^(id data, WVJBResponseCallback responseCallback) {
+        responseCallback(@"Response from pgpEncryptionCallback");
+        
+        NSError* error;
+        NSData* jsonData = [NSJSONSerialization dataWithJSONObject:parameters
+                                                           options:NSJSONWritingPrettyPrinted error:&error];
+        NSString* stringData = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+        
+        id dict = @{ @"message": stringData, @"pgp_public_key" : pgpPublicKey };
+        
+        [self.bridge callHandler:@"pgpEncryptionHandler" data:dict responseCallback:^(id response) {
+            completionBlock(response);
+        }];
+    }];
+    
+    [self fnLoadPGPHandlerPage:webView];
+    [webView setHidden:YES];
+    
+}
+
+- (void)fnLoadPGPHandlerPage:(UIWebView*)webView {
+    
+    NSBundle *frameWorkBundle = [NSBundle bundleForClass:[self class]];
+    NSString *htmlPath = [frameWorkBundle pathForResource:@"PgpHandler.html" ofType:nil];
+    NSString* appHtml = [NSString stringWithContentsOfFile:htmlPath encoding:NSUTF8StringEncoding error:nil];
+    NSURL *baseURL = [NSURL fileURLWithPath:htmlPath];
+    [webView loadHTMLString:appHtml baseURL:baseURL];
 }
 
 @end
--- flutter_to_native_communication/lib/main.dart
+++ flutter_to_native_communication/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+
 
 void main() => runApp(MyApp());
 
@@ -28,7 +30,16 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   static const platform = const MethodChannel('com.LW2/app');
   String encryptedData = "Empty";
+  String publicKey = "";
 
+
+ // Widget life cycle - (Init)
+  @override
+  void initState() {
+    super.initState();
+     getPGPPublicKey();
+
+  }
     @override
   Widget build(BuildContext context) {
     return Material(
@@ -49,11 +60,33 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
+  
+   Future<void> getPGPPublicKey() async {
+    http.Response response = await http.get(Uri.parse('https://staging.litmusworld.com/rateus/pb-key'),);
+    if (response.statusCode == 200) {
+    } else {
+      // If the server did not return a 200 OK response, then throw an exception.
+      throw Exception('Failed to load');
+    }
+     setState(() {
+       publicKey = response.body;
+     });
+
+    //return response.body;
+  }
+
+  static getPGPPrivateKey() async {
+    http.Response response = await http.get(Uri.parse(
+        'https://staging.litmusworld.com/rateus/pv-key'),);
+        print(response.body);
+    return response.body;
+  }
+
 
   Future<void> _invokeObjectiveCMethod() async {
     String data;
     try {
-      final String result = await platform.invokeMethod('encryptData', ["Pooja","Test123"]);
+      final String result = await platform.invokeMethod('encryptData', [publicKey,"Test123"]);
 
       data = '$result';
     } on PlatformException catch (e) {
